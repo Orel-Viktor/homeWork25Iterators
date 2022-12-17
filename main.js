3023e10f755b4c40c5024f90b77950306ffc3195
Orel-Viktor <OrelViktor93@gmail.com>
2022-12-17 15:51:51 +0200

    first commit

--- main.js
+++ main.js
@@ -1,53 +1,74 @@
-"use strict"
+// "use strict"
 
 
-let object =
-{
-    a: 10,
-    b: 20,
-    c: 30,
-    d: 40,
-}
+// let object =
+// {
+//     a: 10,
+//     b: 20,
+//     c: 30,
+//     d: 40,
+// }
 
-object[Symbol.iterator] = function () {
-    this.a = 0
-    return {
-        current: this.a,
-        last: this.d,
+// object[Symbol.iterator] = function () {
+//     this.a = 0
+//     return {
+//         current: this.a,
+//         last: this.d,
 
-        next: function () {
-            if (this.current < this.last) {
-                return {
-                    done: false,
-                    value: this.current += 10
-                }
-            } else {
-                return {
-                    done: true,
-                    value: undefined
-                }
-            }
-        }
-    }
-}
-
-for (let values of object) {
-    console.log(values)
-}
+//         next: function () {
+//             if (this.current < this.last) {
+//                 return {
+//                     done: false,
+//                     value: this.current += 10
+//                 }
+//             } else {
+//                 return {
+//                     done: true,
+//                     value: undefined
+//                 }
+//             }
+//         }
+//     }
+// }
 
 
 
-let object1 = {
+let object = {
     name: 'Stanley',
     Job: 'Web dev',
     age: 28,
 }
-let arr = Object.entries(object1)
 
-for (let map of arr) {
-    map = new Map(arr)
-    let newArr = Object.fromEntries(map)
-    newArr.name = "Viktor"
-    console.log(newArr)
-    
+object[Symbol.iterator] = function () {
+    let keys = Object.keys(this)
+    this.i = 0
+    return {
+        current: this.i,
+        next: function () {
+            if (keys[this.current] !== keys[keys.length]) {
+                return {
+                    done: false,
+                    value: keys[this.current++]
+                }
+            } else {
+                return {
+                    done: true,
+                    value: undefined,
+                }
+            }
+
+        }
+    }
 }
+
+
+
+console.log(object[Symbol.iterator]().next())
+console.log(object[Symbol.iterator]().next())
+console.log(object[Symbol.iterator]().next())
+console.log(object[Symbol.iterator]().next())
+
+
+// for (let keys of object) {
+//     console.log(keys)
+// }
