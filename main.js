ba7696e8f5c5e2fad8c1b78e1429f5dd0e465176
Orel-Viktor <OrelViktor93@gmail.com>
2022-12-18 14:39:41 +0300

    part2

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // "use strict"
 
-
 // let object =
 // {
 //     a: 10,
@@ -31,46 +30,48 @@
 //     }
 // }
 
-
-
 let object = {
-    name: 'Stanley',
-    Job: 'Web dev',
-    age: 28,
-}
+  name: "Stanley",
+  Job: "Web dev",
+  age: 28,
+};
 
 object[Symbol.iterator] = function () {
-    let entries = Object.entries(this);
+  let entries = Object.entries(this);
+  return {
+    current: 0,
+    next: function () {
+      if (entries[this.current] !== undefined) {
+        return {
+          done: false,
+          value: entries[this.current++],
+        };
+      } else {
+        return {
+          done: true,
+          value: undefined,
+        };
+      }
+    },
+  };
+};
 
-    return {
-        current: 0,
-        next: function () {
-            if (entries[this.current] !== entries[entries.length]) {
-                return {
-                    done: false,
-                    value: entries[this.current++]
-                }
-            } else {
-                return {
-                    done: true,
-                    value: undefined,
-                }
-            }
 
-        }
-    }
-}
-
+let iterObj = object[Symbol.iterator]()
+console.log(iterObj.next())
+console.log(iterObj.next())
+console.log(iterObj.next())
+console.log(iterObj.next())
 
 for (let [key, value] of object) {
-    const p = document.createElement('p');
-    const b = document.createElement('strong');
-    const s = document.createElement('span');
+  const p = document.createElement("p");
+  const b = document.createElement("strong");
+  const s = document.createElement("span");
 
-    b.innerText = key.toUpperCase() + ': ';
-    s.innerText = value;
+  b.innerText = key.toUpperCase() + ": ";
+  s.innerText = value;
 
-    p.appendChild(b);
-    p.appendChild(s);
-    document.body.appendChild(p)
+  p.appendChild(b);
+  p.appendChild(s);
+  document.body.appendChild(p);
 }
